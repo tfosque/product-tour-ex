0863b690df1abb28a2b5a85feee77a6ee061a2ad
Tim Fosque <tfosque@gmail.com>
2021-09-24 09:27:26 -0500

    wip3

--- src/app/components/product-tour/product-tour-stepper/product-tour-stepper.component.ts
+++ src/app/components/product-tour/product-tour-stepper/product-tour-stepper.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-product-tour-stepper',
   templateUrl: './product-tour-stepper.component.html',
-  styleUrls: ['./product-tour-stepper.component.scss']
+  styleUrls: ['./product-tour-stepper.component.scss'],
 })
-export class ProductTourStepperComponent implements OnInit {
+export class ProductTourStepperComponent implements OnInit, AfterViewInit {
+  @ViewChild('stepper') stepper: MatStepper;
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) {
     this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      firstCtrl: ['', Validators.required],
     });
     this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+      secondCtrl: ['', Validators.required],
     });
   }
 
-  ngOnInit() {
-
+  ngOnInit() {}
+  ngAfterViewInit() {
+    this.stepper.selectedIndex = 0;
   }
 
   buttonClick(input: any) {
-    console.log('recv emitter', { input })
+    console.log('recv emitter', { input });
   }
 }
